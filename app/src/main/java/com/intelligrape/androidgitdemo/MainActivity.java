b40f0e1222dce1a38c3032df2244c409e6104edd
Shalini Prajesh <shalinip@intelligrape.com>
2015-06-27 11:45:42 +0530

    buttonScreen3 added

--- app/src/main/java/com/intelligrape/androidgitdemo/MainActivity.java
+++ app/src/main/java/com/intelligrape/androidgitdemo/MainActivity.java
@@ -1,19 +1,32 @@
 package com.intelligrape.androidgitdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * MainActivity of the class
  */
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonScreen3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonScreen3 = (Button) findViewById(R.id.button_screen3);
+        buttonScreen3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Screen3Activity.class));
+            }
+        });
+
+
     }
 
     @Override
