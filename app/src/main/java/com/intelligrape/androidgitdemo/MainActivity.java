6287bdf353bddbdb07dc2a1f4051373866e28e8c
Rahul Agarwal <rahul25101987@gmail.com>
2015-06-27 11:39:07 +0530

    Main Screen link

--- app/src/main/java/com/intelligrape/androidgitdemo/MainActivity.java
+++ app/src/main/java/com/intelligrape/androidgitdemo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.intelligrape.androidgitdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_screen2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Screen2.class));
+            }
+        });
     }
 
     @Override
