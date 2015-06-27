73fd63d94ee9c441853da3412d1c8c30363cf350
iamitksharma <suchit.kapoor@tothenew.com>
2015-06-27 11:11:15 +0530

    Screen2 Activity Added

---
+++ app/src/main/java/com/intelligrape/androidgitdemo/Screen2.java
@@ -0,0 +1,15 @@
+package com.intelligrape.androidgitdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ttnd on 27/6/15.
+ */
+public class Screen2 extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
