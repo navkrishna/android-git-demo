1057dd53a26411a9f1a00adf67c9b1d67511cb14
noor <noor.alam@intelligrape.com>
2015-06-27 11:18:42 +0530

    button text chagned and listerner added

--- app/src/main/java/com/intelligrape/androidgitdemo/Screen5_Activity.java
+++ app/src/main/java/com/intelligrape/androidgitdemo/Screen5_Activity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class Screen5_Activity extends Activity {
@@ -15,6 +16,11 @@ public class Screen5_Activity extends Activity {
         setContentView(R.layout.screen5);
 
         btn_screen5 = (Button) findViewById(R.id.button_screen5);
+
+    }
+
+
+    public void goToScreen6(View v){
 
     }
 
