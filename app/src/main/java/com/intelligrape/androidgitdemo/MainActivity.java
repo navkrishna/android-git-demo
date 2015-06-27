8e1966793d88d7fdbd15a170f0f56f1e25fb812d
pankajt <pankaj.tewani@tothenew.com>
2015-06-27 13:00:20 +0530

    Screen button on main activity working

--- app/src/main/java/com/intelligrape/androidgitdemo/MainActivity.java
+++ app/src/main/java/com/intelligrape/androidgitdemo/MainActivity.java
@@ -59,6 +59,13 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        findViewById(R.id.button_redirect_screen7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ScreenSeven.class));
+            }
+        });
+
     }
 
     @Override
